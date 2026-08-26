a4b2e365056c0b6ae9e969fc4a0ec019255872db
Denis Silveira <denis.rayan@gmail.com>
2026-08-26 16:17:40 +0200

    Update diagscope.rb
    updating file

--- Formula/diagscope.rb
+++ Formula/diagscope.rb
@@ -1,7 +1,7 @@
 class Diagscope < Formula
   desc "Static analyzer for Java/Kotlin projects — finds code that goes blind when things break"
   homepage "https://github.com/DiagScope/diagscope"
-  url "https://github.com/DiagScope/diagscope/releases/download/v0.1.0-alpha.1/diagscope-0.1.0-alpha.1.jar"
+  url "https://github.com/DiagScope/diagscope/releases/download/v0.1.0-alpha.2/diagscope-0.1.0-alpha.2.jar"
   sha256 "099d74cb8a1a233843963d590f96b8f2eb81cb7ebf9cace718adb3b8f379d58c"
   version "0.1.0-alpha.2"
   license "Apache-2.0"
